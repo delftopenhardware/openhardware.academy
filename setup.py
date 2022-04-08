d6232b4464ea5d20413802ee84294aa29d365473
jurra <jose.urra86@gmail.com>
2022-04-08 19:16:17 +0200

    Added more specific dependencies

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        'jupyter-book',
-        'ghp-import',
+        'jupyter-book==0.12.0',
+        'ghp-import==2.0.2',
     ]
 )
